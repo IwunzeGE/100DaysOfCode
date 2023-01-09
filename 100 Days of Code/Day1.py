c75d9ded698278e3514ca5c8e54cdecd59a8efa6
IwunzeGE <110903886+IwunzeGE@users.noreply.github.com>
2023-01-09 13:31:06 +0100

    Add files via upload

---
+++ 100 Days of Code/Day1.py
@@ -0,0 +1,13 @@
+#Band Name Generator
+
+#1. Create a greeting for your program
+print("Welcome To The Band Name Generator!\n Fill the details below and we would suggest a band name for you")
+
+#2. Ask the user for the city that they grew up in.
+city = input("What is the name of the city you grew up in?\n")
+
+#3. Ask the user for the name of a pet
+pet = input("What is the name of your pet?\n")
+
+#4. Combine the name of their city and pet name and show them their band name.
+print("Your Band Name could be " + city + " " + pet)
